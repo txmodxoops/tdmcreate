782ab2b0975518c641dafe2714d263636cc1040b
mambax7 <mambax7@gmail.com>
2017-12-04 10:16:31 -0500

    updates

--- language/english/admin.php
+++ language/english/admin.php
@@ -415,3 +415,5 @@ define('_AM_TDMCREATE_FILE_MID_LIST', 'Module Name');
 define('_AM_TDMCREATE_FILE_EXTENSION_LIST', 'Extension Type');
 define('_AM_TDMCREATE_FILE_INFOLDER_LIST', 'In Folder');
 define('_AM_TDMCREATE_FORM_ACTION', 'Action');
+
+define('_AM_TDMCREATE_THEREARENT_SETTINGS', 'There are NO Settings! Please create Settings');
